134845cdb933cccc7098d056948c9d2c927d5abd
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-02-19 23:07:09 +0700

    add get request in user

--- routers/users.php
+++ routers/users.php
@@ -1,33 +1,48 @@
 <?php
-//GET /users//{userId}
+
   function route($method, $urlList, $requestData) { 
       //  echo json_encode($urlList);
      
     
             $userId = $urlList[1];
 
-           // echo json_encode($urlList[1]);
-            
+            //echo json_encode($urlList[1]);
+
+            if($userId==null)
+            {
+                $sql = "SELECT * FROM users"; //GET /users//{userId}
+            }
+            else{
+
+                $sql = "SELECT userId, username,roleId,name,surname FROM users WHERE userId= '$userId'"; //GET /users//{userId}
+            }
             $link = mysqli_connect("127.0.0.1", "backend", "password", "backend");
-            $sql = "SELECT userId, username,roleId,name,surname FROM users WHERE userId= '$userId'";
+            
+                                 
             $result = mysqli_query($link, $sql);
-            $message["users"]=[];
+            $output["users"]=[];
             $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            if ($rows==null){ 
+            echo "Пользователи не найдены!";
+            }
+            else
+            {
 
                 foreach ($rows as $row) {
                 
-                    $message["users"][]=[
+                    $output["users"][]=[
                         "userId"=>$row['userId'],
                         "username"=>$row['username'],
                         "roleId"=>$row['roleId'],
-                        "name"=>$row['usernnameame'],
+                        "name"=>$row['name'],
                         "surname"=>$row['surname']];
                
                 
-                }
-
-                echo json_encode( $message);
+                } 
+                echo json_encode( $output);
     
+            }
+               
             return;
     
 
