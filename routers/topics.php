f99462ab3b4f787bd15b985ba47c304bf7312a37
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-03-01 19:39:28 +0700

    add topic request

--- routers/topics.php
+++ routers/topics.php
@@ -15,24 +15,27 @@ function route($method, $urlList, $requestData)
                 $user = $Link->query("SELECT * FROM users WHERE userId = '$userId'")->fetch_assoc();
 
                 if (!is_null($user)) {
-                    
-                    $result = mysqli_query($Link, "SELECT * FROM `topics`");
-                    $topics = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                    if (is_null($topics)){ 
-                         setHTTPStatus("400", "ПРИДУМАЙ!"); 
-                    }
-                    else
-                    {
-                        echo json_encode( $topics);
-                      
-                    }
-                    
-                } else {
-                    setHTTPStatus("400", "ПРИДУМАЙ!");
-                }
 
+                    if ($urlList[1]) {
+                        $result = mysqli_query($Link, "SELECT * FROM topics WHERE id= $urlList[1] ");
+                        $topic = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                        if (is_null($topic)) {
+                            setHTTPStatus("400", "ПРИДУМАЙ!");
+                        } else {
+                            echo json_encode($topic);
+                        }
+                    } else {
+                        $result = mysqli_query($Link, "SELECT * FROM topics");
+                        $topics = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                        if (is_null($topics)) {
+                            setHTTPStatus("400", "ПРИДУМАЙ!");
+                        } else {
+                            echo json_encode($topics);
+                        }
+                    }
+                }
             } else {
-                echo "404: input data incorrect";
+                setHTTPStatus("403", "Permission denied. Authorization token are invalid");
             }
 
 
@@ -42,26 +45,81 @@ function route($method, $urlList, $requestData)
 
             $name = $requestData->body->name;
             $parentId = $requestData->body->parentId;
-           
-            $taskInsertRezult = $Link->query("INSERT INTO topics( name, parentId) VALUES('$name', '$parentId')");
+
+            $taskInsertRezult = $Link->query("INSERT INTO topics(name, parentId) VALUES('$name', '$parentId')");
 
             if (!$taskInsertRezult) {
-            
+
                 if ($Link->errno == 1062) {
                     setHTTPStatus("409", "Login '$name' is taken");
                     return;
                 }
             } else {
-                
+
                 setHTTPStatus("200", "User '$name' was succesfully created");
             }
 
-           echo json_encode($requestData);
+            echo json_encode($requestData);
 
             break;
+        case 'PATCH':
 
+            if ($urlList[1]) {
+                $findTopicRezult = mysqli_query($Link, "SELECT * FROM topics WHERE id= $urlList[1] ");
+                $topic = mysqli_fetch_all($findTopicRezult, MYSQLI_ASSOC);
+                if (is_null($topic)) {
+                    setHTTPStatus("400", "ПРИДУМАЙ!");
+                } else {
+
+                    $name = $requestData->body->name;
+                    $parentId = $requestData->body->parentId;
+
+                    if (!empty($name) && (!empty($parentId))) {
+                        $name = "name='$name',";
+                        $parentId = "parentId='$parentId'";
+                    } elseif (!empty($name)) {
+                        $name = "name='$name'";
+                    } elseif (!empty($parentId)) {
+                        $parentId = "parentId='$parentId'";
+                    }
+                    $userUpdate = $Link->query("UPDATE topics SET $name $parentId WHERE id= $urlList[1]");
+                    if (!$userUpdate) {
+                        echo json_encode("400");
+                        echo json_encode($Link -> error);
+                    } else {
+                        echo json_encode($topic);
+                    }
+                }
+            }
+            break;
+
+        case 'DELETE':
+        
+            if ($urlList[1]) {
+                $findTopicRezult = mysqli_query($Link, "SELECT * FROM topics WHERE id= $urlList[1] ");
+                $topic = mysqli_fetch_all($findTopicRezult, MYSQLI_ASSOC);
+                if (is_null($topic)) {
+                    setHTTPStatus("400", "ПРИДУМАЙ!");
+                } else {
+                    $sql = "DELETE FROM topics WHERE id= $urlList[1]";
+                    $topicDelete = $Link->query($sql);
+                    if (!$topicDelete) {
+                        echo json_encode($Link->error);
+                    } else {
+                        setHTTPStatus("200", "ОК");
+                    
+                    }
+                   
+                }
+            }
+        
+        
+        
+        
+        
+            break;
         default:
-            
+
             break;
     }
 }
